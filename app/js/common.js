4222b9fd3fb2c5ce833e3464acbd3e9ef1036e2f
AlLosevK <al.losev.k@gmail.com>
2017-12-23 01:50:27 +0300

    Mobile search

--- app/js/common.js
+++ app/js/common.js
@@ -8,6 +8,26 @@ $(".country").click(function (e) {
     $(".country").removeClass('country__active');
     $(this).addClass('country__active');
 })
+//
+//$(".search-mob").click(function (e) {
+//    e.preventDefault();
+//    $(".search-mob").removeClass('search__btn-close');
+//    $(this).addClass('search__btn-open');
+//    $(".search").fadeIn("slow", function () {});
+//})
+
+$(".search-mob").click(function (e) {
+    e.preventDefault();
+    if ($(".search-mob").hasClass(".search__btn-open")) {
+        $(".search-mob").removeClass('search__btn-open');
+        $(".search-mob").addClass('search__btn-close');
+        $(".search").fadeOut(400);
+    } else {
+        $(".search-mob").removeClass('search__btn-close');
+        $(".search-mob").addClass('search__btn-open');
+        $(".search").fadeIn(400);
+    };
+})
 
 jQuery(document).ready(function ($) {
     $("#menu").mmenu({
