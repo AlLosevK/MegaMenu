9dbe89eb87cc3937f45b319d806630cbf4e1126d
AlLosevK <al.losev.k@gmail.com>
2017-12-23 03:09:20 +0300

    All's done

--- app/js/common.js
+++ app/js/common.js
@@ -37,14 +37,6 @@ jQuery(document).ready(function ($) {
         "navbars": [
             {
                 "position": "top"
-                  },
-            {
-                "position": "bottom",
-                "content": [
-                        "<a class='fa fa-envelope' href='#/'></a>",
-                        "<a class='fa fa-twitter' href='#/'></a>",
-                        "<a class='fa fa-facebook' href='#/'></a>"
-                     ]
                   }
                ]
     });
