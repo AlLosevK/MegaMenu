e67359b15cfd87161b7d9e35953635ec0f5d2c12
AlLosevK <al.losev.k@gmail.com>
2017-12-24 22:11:00 +0300

    Build project

---
+++ dist/js/common.js
@@ -0,0 +1,43 @@
+$(".popcountry li").click(function (e) {
+    e.preventDefault();
+    $(".popcountry li").removeClass('visible');
+    $(this).addClass('visible');
+})
+$(".country").click(function (e) {
+    e.preventDefault();
+    $(".country").removeClass('country__active');
+    $(this).addClass('country__active');
+})
+//
+//$(".search-mob").click(function (e) {
+//    e.preventDefault();
+//    $(".search-mob").removeClass('search__btn-close');
+//    $(this).addClass('search__btn-open');
+//    $(".search").fadeIn("slow", function () {});
+//})
+
+$(".search-mob").click(function (e) {
+    e.preventDefault();
+    if ($(this).hasClass('search__btn-open')) {
+        $(this).removeClass('search__btn-open');
+        $(this).addClass('search__btn-close');
+        $(".search").fadeOut(400);
+    } else {
+        $(this).removeClass('search__btn-close');
+        $(this).addClass('search__btn-open');
+        $(".search").fadeIn(400);
+    };
+})
+
+jQuery(document).ready(function ($) {
+    $("#menu").mmenu({
+        "offCanvas": {
+            "position": "right"
+        },
+        "navbars": [
+            {
+                "position": "top"
+                  }
+               ]
+    });
+});
